447f25917c5db5577b8c5ef098e9a558c1a7f0f1
bryan18jonah <bryan18jonah@gmail.com>
2021-08-24 23:04:51 +0200

    Ssecondary commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container-fluid">
+    <div>
+      <h1>
+        SABI ITAMAE
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.container-fluid {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: $color-dark-default;
+  
+  h1 {
+    color: $color-primary-default;
+  }
+}
+</style>
